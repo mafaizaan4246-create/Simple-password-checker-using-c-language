822dffa8074872ce54444ac527832ed93d5148db
Mohammed Abdul Faizan <mafaizaan4246@gmail.com>
2026-03-20 23:47:07 +0530

    Correct password input handling and cleanup
    Fix password input and remove 'finally' block.

--- simplepasschecker.c
+++ simplepasschecker.c
@@ -4,7 +4,7 @@ int main(){
 char password[50];
 int length;
 printf("Enter your password to check:");
-scanf("%s",&password);
+scanf("%s",password);
 length=strlen(password);
 if(length<7){
 printf("Your password is too weak!!");
@@ -14,8 +14,6 @@ printf("Your password is moderate!!");
 }
 else{
 printf("Your password is strong!!");
-}
-finally{
 printf("\nThank you for using our password checker!!");
 }
 }

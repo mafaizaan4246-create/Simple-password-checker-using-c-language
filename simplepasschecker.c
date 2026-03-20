14ed41954643f3452d07cf1d900178a7e1731742
Mohammed Abdul Faizan <mafaizaan4246@gmail.com>
2026-03-20 23:37:07 +0530

    Add simple password strength checker in C

---
+++ simplepasschecker.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char password[50];
+int length;
+printf("Enter your password to check:");
+scanf("%s",&password);
+length=strlen(password);
+if(length<7){
+printf("Your password is too weak!!");
+}
+else if(length>=7&&length<=11){
+printf("Your password is moderate!!");
+}
+else{
+printf("Your password is strong!!");
+}
+}
